0e95dc48a079ee2c53e0cd8266a57feac10b5bb7
kosmas9 <34135515+kosmas9@users.noreply.github.com>
2025-02-25 18:10:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,14 @@ host="tempraturestestdb.database.windows.net"
 
 # Define database connection string (replace with your connection details)
 DATABASE_URL = f"mssql+pyodbc://{username}:{password}@{host}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
+# Create database engine
+engine = create_engine(DATABASE_URL)
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, Kosmas!"}
-
-# Create database engine
-engine = create_engine(DATABASE_URL)
-
-app = FastAPI()
 
 @app.get("/locations")
 def list_locations():
